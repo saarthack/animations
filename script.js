b4406cc62be7bcab39c9f3fbcfe649df8bdbbf3c
Sarthak Sharma <sarthaksharma@Sarthaks-Laptop.local>
2024-06-28 19:17:58 +0530

    basics covered

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+// GSAP - GreenSock Animation Platform 
+// JavaScript Animation Library 
+
+// gsap.to() make animation initial to final 
+
 gsap.to("#box",{
-    x:800,
+    left:"70%",
     duration:2,
-    delay:1
+    delay:1,
+    rotate:360,
+    backgroundColor:"red"
 })
+
+// gsap.from() makes your animation final to initial 
+
+// gsap.from("#box",{
+//     left:"70%",
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"red"
+// })
+
+// gsap.to("#box",{
+//     x:750,
+//     duration:2,
+//     delay:1,
+//     scale:0.5,
+//     repeat:-1,
+//     yoyo:true
+// })
+
+
+// gsap.from("#box",{
+//     y:200,
+//     opacity:0,
+//     duration:1,
+//     delay:1
+// })
